8b21cb8d6057faa07c59685b376405e844fecbaf
Tomáš Vojtášek <vojtasek.tomas@seznam.cz>
2018-01-30 10:15:22 +0100

    RELATED: FET-1 Update dependencies (#26)

---
+++ javascript/test.js
@@ -0,0 +1,13 @@
+const a = '1';
+
+export default {
+    a
+};
+
+
+// Jest test globals
+describe('something', () => {
+    it('is very interestring', () => {
+        expect(true).toEqual(true);
+    });
+});
